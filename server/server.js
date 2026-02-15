3afe7bf17c558629a3a7791d88c74287d3d77390
ryzenop07 <ajay211st@gmail.com>
2026-02-15 20:13:29 +0530

    new changes

--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
